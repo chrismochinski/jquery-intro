f133e1068815aa40256d6b4f0941161dc838ee88
Chris Mo <cmochinski@gmail.com>
2021-08-26 13:13:39 -0500

    post lunch commit pre .this and .css

--- client.js
+++ client.js
@@ -13,31 +13,48 @@ function readyNow() {
     // code that runs after the dom has loaded
     // jQuery can reference by element, class or ID
     $('h1').html(pageTitle);
-
-
+    $('#clear-button').on('click', clearGroceryItems);
+    $('#add-button').on('click', addGroceryItem);
     displayGroceryItems();
+}
+
+
+function displayGroceryItems(){
     console.log('displaying grocery list');
     clearGroceryItems();
     for (let item of groceryList) {
-        $('#grocery-list').append(
-            `<li class="grocery-item">
-            ${item}
-            </li>`);
+        $('#grocery-list').append(`<li>${item}</li>`);
     }
+}
+
+function addGroceryItem(){ //appends item DIRECTLY to dom in ul/li
+    console.log('add grocery item to groceryList array')
+    let newItem = $('#add-item');
+    console.log(`item to add: ${newItem.val()}`);
+    $('#grocery-list').append(`<li>${newItem.val()}</li>`);
+    $('#add-item').val('');
+    ////////////
+
+
+    // groceryList.push(newItem.val());
+    // newItem.val('');
+    
+
+    // .val();
 }
 
 let isClear = false;
 function clearGroceryItems() {
-    iClear = true; //on clicking button, this is activated
+    isClear = true; //on clicking button, this is activated
     console.log('clear grocery list');
     $('#grocery-list').empty();
 }
 
 
-function displayGroceryItems() {
-    console.log('4. (3 on js) display grocery list');
-    $('.div-a').text('All the divs');
-}
+// function displayGroceryItems() {
+//     console.log('4. (3 on js) display grocery list');
+//     $('.div-a').text('All the divs');
+// }
 
 
 console.log('2. (4 on  js) script loaded - end');
@@ -46,6 +63,7 @@ console.log('2. (4 on  js) script loaded - end');
 // .append('add something at end')
 // .html('replace with HTML')
 // .empty()  ...clear all things, everything that is child too
+// .val() ...setting value (value is an attribute of the input field)
 // $(selector).on('event', functionToCall);
 
 //things that were from the readyNow function:
